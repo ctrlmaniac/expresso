2900e0698c055e94df1cd8870b15e0e9dd34e658
Davide Di Criscito <davide.dicriscito@gmail.com>
2023-10-01 07:45:30 +0200

    fix client path

--- app/server/index.js
+++ app/server/index.js
@@ -20,14 +20,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/media", express.static(path.join(__dirname, "..", "media")));
 
 // Client
-app.use(express.static(path.join(__dirname, "client")));
+app.use(express.static(path.join(__dirname, "..", "client")));
 
 // API or client routes
-app.use("/", helloRouter);
+app.use("/hello", helloRouter);
 
 // Client
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/index.html"));
+  res.sendFile(path.join(__dirname, "..", "client", "index.html"));
 });
 
 app.listen(port, () => {

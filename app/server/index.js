0b959fb4c0b1d716b23f1c251a9519580ac7e1f4
Davide Di Criscito <davide.dicriscito@gmail.com>
2023-10-02 11:46:46 +0200

    rename client into static

--- app/server/index.js
+++ app/server/index.js
@@ -19,15 +19,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Media
 app.use("/media", express.static(path.join(__dirname, "..", "media")));
 
-// Client
-app.use(express.static(path.join(__dirname, "..", "client")));
+// Static files
+app.use(express.static(path.join(__dirname, "..", "static")));
 
 // API or client routes
 app.use("/hello", helloRouter);
 
-// Client
+// Serve index.html
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "client", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "static", "index.html"));
 });
 
 app.listen(port, () => {
